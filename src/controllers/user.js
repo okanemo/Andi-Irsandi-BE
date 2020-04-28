6a7933a82966f013c1139c8b9b630a052f028061
andi-irsandi <andi.ramadani765@gmail.com>
2020-04-28 15:26:35 +0700

    be

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,86 +1,92 @@
-const JWT = require('jsonwebtoken')
-const userModel = require('../models/user')
-const helper = require('../helpers')
-const { JWT_KEY } = require('../configs')
+const JWT = require("jsonwebtoken");
+const userModel = require("../models/user");
+const helper = require("../helpers");
+const { JWT_KEY } = require("../configs");
 
 module.exports = {
-    users: async (req,res)=>{
-        try{
-            const result = await userModel.getUsers()
-            helper.response(res, 200, result)
-        } catch(error){
-            helpers.customErrorResponse(res, 500, 'Failed')
-        }
-    },
-    register: async (req, res) => {
-        try {
-            const salt = helper.generateSalt(18)
-            const hashPassword = helper.setPassword(req.body.password, salt)
-            const data = {
-                name: req.body.name,
-                email: req.body.email,
-                status: 'cashier',
-                salt: hashPassword.salt,
-                password: hashPassword.passwordHash,
-                created_at: new Date(),
-                updated_at: new Date()
-            }
-            const result = await userModel.register(data)
-            helper.response(res, 200, 'user has been added')
-        } catch (error) {
-            console.log(error)
-            helpers.customErrorResponse(res, 500, 'Failed')
-        }
-    },
-    login: async (request, response) => {
-        const data = {
-            password: request.body.password,
-            email: request.body.email
-        }
-
-        const emailValid = await userModel.checkEmail(data.email)
-        if(emailValid.length < 1) response.json({ error: 'Wrong Email' })
-        const dataUser = emailValid[0]
-        const hashPassword = helper.setPassword(data.password, dataUser.salt)
-
-        if (hashPassword.passwordHash === dataUser.password) {
-            const token = JWT.sign({
-                email: dataUser.email,
-                id: dataUser.id
-            }, JWT_KEY, { expiresIn: '1d' })
-
-            delete dataUser.salt
-            delete dataUser.password
-
-            dataUser.token = token
-            helper.response(response, 200, dataUser)
-        } else {
-            response.json({ message: 'Login error!' })
-        }
-    },
-    deleteUser: async (req, res) => {
-        try {
-            const userId = req.params.userId
-            const result = await userModel.deleteUser(userId)
-            helper.response(res, 200, userId)
-        } catch (error) {
-            console.log(error)
-            helper.customErrorResponse(res, 500, 'Server error!')
-        }
-    },
-    updateUser: async (req, res) => {
-        try {
-            const userId = req.params.userId
-            const data = {
-                name: req.body.name,
-                status: req.body.status
-            }
-            await userModel.updateUser(data,userId)
-            const result = await userModel.getDetail(userId)
-            helper.response(res, 200, result[0])
-        } catch (error) {
-            console.log(error)
-            helper.customErrorResponse(res, 500, 'Server error!')
-        }
+  users: async (req, res) => {
+    try {
+      const result = await userModel.getUsers();
+      helper.response(res, 200, result);
+    } catch (error) {
+      helpers.customErrorResponse(res, 500, "Failed");
     }
-}
+  },
+  register: async (req, res) => {
+    try {
+      const salt = helper.generateSalt(18);
+      const hashPassword = helper.setPassword(req.body.password, salt);
+      const data = {
+        name: req.body.name,
+        email: req.body.email,
+        status: "waiting",
+        salt: hashPassword.salt,
+        password: hashPassword.passwordHash,
+        created_at: new Date(),
+        updated_at: new Date(),
+      };
+      const result = await userModel.register(data);
+      helper.response(res, 200, "user has been added");
+    } catch (error) {
+      console.log(error);
+      helpers.customErrorResponse(res, 500, "Failed");
+    }
+  },
+  login: async (request, response) => {
+    const data = {
+      password: request.body.password,
+      email: request.body.email,
+    };
+
+    const emailValid = await userModel.checkEmail(data.email);
+    if (emailValid.length < 1) response.json({ error: "Wrong Email" });
+    const dataUser = emailValid[0];
+    const hashPassword = helper.setPassword(data.password, dataUser.salt);
+
+    if (hashPassword.passwordHash === dataUser.password) {
+      const token = JWT.sign(
+        {
+          email: dataUser.email,
+          id: dataUser.id,
+        },
+        JWT_KEY,
+        { expiresIn: "1m" }
+      );
+
+      delete dataUser.salt;
+      delete dataUser.password;
+
+      dataUser.token = token;
+      const decoded = JWT.decode(token, { complete: true });
+      console.log(decoded.signature);
+      helper.response(response, 200, dataUser);
+    } else {
+      response.json({ message: "Login error!" });
+    }
+  },
+  deleteUser: async (req, res) => {
+    try {
+      const userId = req.params.userId;
+      const result = await userModel.deleteUser(userId);
+      helper.response(res, 200, userId);
+    } catch (error) {
+      console.log(error);
+      helper.customErrorResponse(res, 500, "Server error!");
+    }
+  },
+  updateUser: async (req, res) => {
+    try {
+      const userId = req.params.userId;
+      const data = {
+        name: req.body.name,
+        status: req.body.status,
+      };
+      await userModel.updateUser(data, userId);
+      const result = await userModel.getDetail(userId);
+      helper.response(res, 200, result[0]);
+    } catch (error) {
+      console.log(error);
+      helper.customErrorResponse(res, 500, "Server error!");
+    }
+  },
+};
--- src/routes/product.js
+++ src/routes/product.js
@@ -1,14 +1,19 @@
-const express = require('express')
-const Route = express.Router()
+const express = require("express");
+const Route = express.Router();
 
-const { authentication, authorization } = require('../helpers/auth')
-const { getAll, addProduct, updateProduct, detailProduct, deleteProduct } = require('../controllers/product')
+const { authentication, authorization } = require("../helpers/auth");
+const {
+  getAll,
+  addProduct,
+  updateProduct,
+  detailProduct,
+  deleteProduct,
+} = require("../controllers/product");
 
-Route
-    .get('/', authentication, authorization, getAll)
-    .get('/:productId', authentication, authorization, detailProduct)
-    .post('/', authentication, authorization, addProduct)
-    .patch('/:productId', authentication, authorization, updateProduct)
-    .delete('/:productId', authentication, authorization, deleteProduct)
+Route.get("/", authentication, authorization, getAll)
+  .get("/:productId", authentication, authorization, detailProduct)
+  .post("/", authentication, authorization, addProduct)
+  .patch("/:productId", authentication, authorization, updateProduct)
+  .delete("/:productId", authentication, authorization, deleteProduct);
 
-module.exports = Route
+module.exports = Route;
